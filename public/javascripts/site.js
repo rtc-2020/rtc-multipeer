39080b323241e0e829f479f22f43b34f01412a00
Karl Stolley <karl.stolley@gmail.com>
2020-12-02 17:29:06 -0600

    Add basically diagnostic signaling events and handlers

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -9,6 +9,28 @@ var self = {
   id: ''
 };
 
+sc.on('connect', handleSignalingConnection);
+sc.on('connected peers', handleConnectedPeers);
+sc.on('new connected peer', handleNewConnectedPeer);
+
+// Set connection diagnostic and self_id assignment
+function handleSignalingConnection() {
+  // Set self.id
+  self.id = sc.id;
+  if (self.DEBUG) console.log('My ID on the signaling channel is', self.id);
+}
+
+// Handle any already connected peers
+function handleConnectedPeers(peers) {
+  if (self.DEBUG) console.log('Connected peers:', peers);
+  sc.emit('new connected peer', self.id);
+}
+
+// Handle newly connected peers
+function handleNewConnectedPeer(peer) {
+  if (self.DEBUG) console.log('Newly connected peer:', peer);
+}
+
 // Join button
 var joinButton = document.querySelector('#join-call');
 joinButton.addEventListener('click', joinCall);
