d10f3babd2182c1d79ac2011242a3f50c8f78a1c
Karl Stolley <karl.stolley@gmail.com>
2020-12-02 17:24:02 -0600

    Create a `self` object to hold details about self

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -3,6 +3,12 @@
 // See https://socket.io/docs/v3/client-api/index.html#new-Manager-url-options
 var sc = io('/' + NAMESPACE, { autoConnect: false });
 
+// Object to hold details about self
+var self = {
+  DEBUG: true,
+  id: ''
+};
+
 // Join button
 var joinButton = document.querySelector('#join-call');
 joinButton.addEventListener('click', joinCall);
