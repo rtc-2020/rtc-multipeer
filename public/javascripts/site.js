b476261e8e5d5164a95a436743c48e9b095aa6e1
Karl Stolley <karl.stolley@gmail.com>
2020-12-02 17:23:04 -0600

    Manually open the connection to the signaling server

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,1 +1,15 @@
+// Use `sc` for the signaling channel.
+// DO NOT connect automatically; wait until Join button is clicked
+// See https://socket.io/docs/v3/client-api/index.html#new-Manager-url-options
+var sc = io('/' + NAMESPACE, { autoConnect: false });
 
+// Join button
+var joinButton = document.querySelector('#join-call');
+joinButton.addEventListener('click', joinCall);
+
+function joinCall(event) {
+  // Open the signaling channel connection
+  sc.open();
+  // Remove the join button
+  event.target.remove();
+}
