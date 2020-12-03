1fd93bd4a8c13d65d2ce46463f6a407bbc71b230
Karl Stolley <karl.stolley@gmail.com>
2020-12-03 12:21:43 -0600

    Put more console.log calls behind self.DEBUG

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -72,7 +72,7 @@ async function handleSignal({ to, from, candidate, description }) {
 
       // Set the remote description...
       try {
-        console.log('Trying to set a remote description:\n', description);
+        if (self.DEBUG) console.log('Trying to set a remote description:\n', description);
         clientIs.settingRemoteAnswerPending = description.type == "answer";
         await pc.setRemoteDescription(description);
         clientIs.settingRemoteAnswerPending = false;
@@ -82,7 +82,7 @@ async function handleSignal({ to, from, candidate, description }) {
 
       // ...if it's an offer, we need to answer it:
       if (description.type == 'offer') {
-        console.log('Specifically, an offer description...');
+        if (self.DEBUG) console.log('Specifically, an offer description...');
           try {
             // Very latest browsers are totally cool with an
             // argument-less call to setLocalDescription:
@@ -91,26 +91,28 @@ async function handleSignal({ to, from, candidate, description }) {
             // Older (and not even all that old) browsers
             // are NOT cool. So because we're handling an
             // offer, we need to prepare an answer:
-            console.log('Falling back to older setLocalDescription method when receiving an offer...');
+            if (self.DEBUG) console.log('Falling back to older setLocalDescription method when receiving an offer...');
             if (pc.signalingState == 'have-remote-offer') {
               // create a answer, if that's what's needed...
-              console.log('Trying to prepare an answer:');
+              if (self.DEBUG) console.log('Trying to prepare an answer:');
               var offer = await pc.createAnswer();
             } else {
               // otherwise, create an offer
-              console.log('Trying to prepare an offer:');
+              if (self.DEBUG) console.log('Trying to prepare an offer:');
               var offer = await pc.createOffer();
             }
             await pc.setLocalDescription(offer);
           } finally {
-            console.log('Sending a response:\n', pc.localDescription);
+            if (self.DEBUG) console.log('Sending a response:\n', pc.localDescription);
             sc.emit('signal', { to: from, from: self_id, description: pc.localDescription });
           }
       }
 
     } else if (candidate) {
-        console.log('Received a candidate:');
-        console.log(candidate);
+        if (self.DEBUG) {
+          console.log('Received a candidate:');
+          console.log(candidate);
+        }
         // Save Safari and other browsers that can't handle an
         // empty string for the `candidate.candidate` value:
         try {
@@ -190,11 +192,11 @@ function negotiateConnection(peer_id) {
         // Older (and not even all that old) browsers
         // are NOT cool. So because we're making an
         // offer, we need to prepare an offer:
-        console.log('Falling back to older setLocalDescription method when making an offer...');
+        if (self.DEBUG) console.log('Falling back to older setLocalDescription method when making an offer...');
         var offer = await pc.createOffer();
         await pc.setLocalDescription(offer);
       } finally {
-        console.log('Sending an offer:\n', pc.localDescription);
+        if (self.DEBUG) console.log('Sending an offer:\n', pc.localDescription);
         sc.emit('signal', { to: peer_id, from: self.id, description: pc.localDescription });
       }
     } catch(error) {
